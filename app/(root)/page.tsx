cedabdfc83830ae6dda4dcb05adad2ad572aeac3
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-05-17 07:21:32 +0200

    Implement the right side bar and complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,38 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Yuriy' };
+  const loggedIn = {
+    firstName: 'Yuriy',
+    lastName: 'Chamkoriyski',
+    email: 'yuriyc@xoommit.com',
+  };
 
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || 'Guest'}
-          subtext="Access and manage your accounts and transactions in one place efficiently."
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your accounts and transactions in one place efficiently."
+          />
 
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250.35 }, { currentBalance: 750.85 }]}
+      />
     </section>
   );
 };
